a108631495455401e8796bde63995205cdeeee6c
stevenhy <preklady@gmail.com>
2016-02-05 19:35:16 +0000

    Moved the DAO initialization to the describe ouside of context, implemented orders.js, next step is to try to make the tests async

--- test/customer_displays_order.js
+++ test/customer_displays_order.js
@@ -7,25 +7,40 @@ let orderSystemWith = require("../lib/orders");
 /* global context */
 // the feature name should follow the format <ROLE> _ <ACTION> _ <ENTITY>
 describe('Customer displays order', function() {
+    beforeEach(function() {
+        this.orderDAO = {
+            byId : sinon.stub(),
+        },
+        this.orderSystem = orderSystemWith(this.orderDAO);
+    });
     // now add some scenarios
     context("Given that the order is empty", function() {
         beforeEach(function() {
-            let orderDAO = {
-                byId : sinon.stub(),
-            },
-            orderSystem = orderSystemWith(orderDAO);
-            
-            orderDAO.byId.withArgs('some empty order id').returns([]);
-            
-            this.result = orderSystem.display('some empty order id');
+            this.orderId = 'some empty order id';
+            this.orderDAO.byId.withArgs(this.orderId).returns([]);
+            this.result = this.orderSystem.display('some empty order id');
         });
-        it('will show no order items');
-        it('will show 0 as the total price');
+        it('will show no order items', function(){
+            expect(this.result).to.have.property('items').that.is.empty;
+        });
+        it('will show 0 as the total price', function(){
+            expect(this.result).to.have.property('totalPrice').that.is.equal(0);
+        });
         // it('will not be possible to place the order');
         // it('will be possible to add a bverage');
         // it('will not be possible to remove a beverage');
         // it('will not be possible to change the quantity of the beverage');
-        it('will only be possible to add a beverage'); // this sums up the test cases above into one general case
+        it('will only be possible to add a beverage', function(){
+            expect(this.result).to.have.property('actions').that.is.deep.equal([{
+                    action: 'append-beverage',
+                    target: this.orderId,
+                    parameters: {
+                        beverageRef: null,
+                        quantity: 0
+                    }
+                }
+            ]);
+        }); // this sums up the test cases above into one general case
     });
     
     context("Given that the order contains beverages", () => {
